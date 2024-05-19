519e2745b331d982ed4296031b44f01ea17850fb
Rosamon <55803598+Rosamon@users.noreply.github.com>
2024-05-19 16:13:52 +0300

    Again and agian

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,14 @@
 CREATE USER repl_user REPLICATION LOGIN PASSWORD 'Qq12345';
 
 CREATE TABLE public.emails (
-    id integer NOT NULL,
-    email character varying(100) NOT NULL
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE public.phones (
-    id integer NOT NULL,
-    phone character varying(100) NOT NULL
+    id SERIAL PRIMARY KEY,
+    phone VARCHAR(50) NOT NULL
 );
 
 INSERT INTO emails (email) VALUES ('email@test.test'), ('r.klimov@innopolis.university');
-INSERT INTO phone_numbers (phone_number) VALUES ('+7(911) 123-45-67'), ('89111234567');
+INSERT INTO phones (phone) VALUES ('+7(911) 123-45-67'), ('89111234567');
