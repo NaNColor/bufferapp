fe35140a234b5067a9f42f8b1d74896964b35e49
Rosamon <55803598+Rosamon@users.noreply.github.com>
2024-05-19 14:56:10 +0300

    Again and agian

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 CREATE USER repl_user REPLICATION LOGIN PASSWORD 'Qq12345';
 
-CREATE DATABASE pt_db;
+CREATE DATABASE IF NOT EXISTS pt_db;
 
 \c pt_db;
 
@@ -14,12 +14,5 @@ CREATE TABLE public.phones (
     phone character varying(100) NOT NULL
 );
 
-COPY public.emails (id, email) FROM stdin;
-1	email@test.test
-2	r.klimov@innopolis.university
-\.
-
-COPY public.phones (id, phone) FROM stdin;
-1	+7(911) 123-45-67
-2	89111234567
-\.
+INSERT INTO emails (email) VALUES ('email@test.test'), ('r.klimov@innopolis.university');
+INSERT INTO phone_numbers (phone_number) VALUES ('+7(911) 123-45-67'), ('89111234567');
